119ad2e041377f2f31c25eed67cbef03a5c6f63c
Presto <khy4088@gmail.com>
2022-12-08 15:17:39 +0900

    organize files

--- solving_now/1179.py
+++
@@ -1,3 +0,0 @@
-import sys
-from collections import deque
-N, K = map(int, sys.stdin.readline().rstrip().split())
--- solving_now/19583.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-S, E, Q = map(str, sys.stdin.readline().rstrip().split())
-
-while True:
-    time, name = map(str, sys.stdin.readline().rstrip().split())
